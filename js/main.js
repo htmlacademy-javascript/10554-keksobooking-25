4d960a61fae4a6e65c81cbb88152a9e12a68a8ad
Minor353 <nikitenkogv.main@gmail.com>
2022-03-15 14:26:49 +0200

    Made module7-task2. Created 'control-form' module. Created 'enableInactiveState' and 'enableActiveState' functions in the 'control-form' module

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-import {createCard} from './create-card.js';
+import { createCard } from './create-card.js';
+import { getSimilarObjects } from './data.js';
 
-createCard();
+const similarCards = getSimilarObjects();
+
+createCard(similarCards[0]);
+
