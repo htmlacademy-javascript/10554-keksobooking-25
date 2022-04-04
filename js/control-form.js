c794499af207116d3fd47732e3653d909f13d6e4
Minor353 <nikitenkogv.main@gmail.com>
2022-04-04 11:28:29 +0300

    Made module11-task1

--- js/control-form.js
+++ js/control-form.js
@@ -1,7 +1,14 @@
+import { pristine } from './form-validation.js';
+import { sendData } from './data-api.js';
+import { onSuccessSubmit, onErrorSubmit } from './control-msg.js';
+import { startLatData, startLngData, map, mainMarker } from './leaflet-map.js';
+
 const mainForm = document.querySelector('.ad-form');
 const mainFormChildren = mainForm.querySelectorAll('fieldset');
 const mapFilter = document.querySelector('.map__filters');
 const mapFilterChildren = mapFilter.querySelectorAll('.map__filter');
+const submitButton = mainForm.querySelector('.ad-form__submit');
+const resetButton = mainForm.querySelector('.ad-form__reset');
 
 const enableInactiveState = () => {
   mainForm.classList.add('ad-form--disabled');
@@ -25,4 +32,59 @@ const enableActiveState = () => {
   });
 };
 
-export {enableInactiveState, enableActiveState};
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Публикую...';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+const resetForms = () => {
+  document.querySelector('.ad-form').reset();
+  document.querySelector('.map__filters').reset();
+  mainMarker.setLatLng({
+    lat: startLatData,
+    lng: startLngData,
+  });
+  map.setView({
+    lat: startLatData,
+    lng: startLngData,
+  }, 12);
+  document.querySelector('[name="address"]').value = `${startLatData}, ${startLngData}`;
+};
+
+const resetFormByButton = () => {
+  resetButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    resetForms();
+  });
+};
+
+const sendUserForm = () => {
+  mainForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+    blockSubmitButton();
+    if (isValid) {
+      sendData(
+        () => {
+          onSuccessSubmit();
+          unblockSubmitButton();
+          resetForms();
+        },
+        () => {
+          onErrorSubmit();
+          unblockSubmitButton();
+        },
+        new FormData(evt.target));
+    } else {
+      unblockSubmitButton();
+    }
+  });
+};
+
+
+export {enableInactiveState, enableActiveState, sendUserForm, resetFormByButton};
--- js/leaflet-map.js
+++ js/leaflet-map.js
@@ -1,16 +1,22 @@
 import {enableInactiveState, enableActiveState} from './control-form.js';
 import { createCard } from './create-card.js';
-import { getSimilarObjects } from './data.js';
+import { getData } from './data-api.js';
+import {onErrorGetServer} from './control-msg.js';
 
 enableInactiveState();
+
+const startLatData = 35.69756;
+const startLngData = 139.76655;
+
 
 const map = L.map('map-canvas')
   .on('load', () => {
     enableActiveState();
+    document.querySelector('[name="address"]').value = `${startLatData}, ${startLngData}`;
   })
   .setView({
-    lat: 35.697567,
-    lng: 139.766556,
+    lat: startLatData,
+    lng: startLngData,
   }, 12);
 
 L.tileLayer(
@@ -29,8 +35,8 @@ const mainPinIcon = L.icon({
 
 const mainMarker = L.marker(
   {
-    lat: 35.697567,
-    lng: 139.766556,
+    lat: startLatData,
+    lng: startLngData,
   },
   {
     draggable: true,
@@ -41,11 +47,9 @@ const mainMarker = L.marker(
 mainMarker.addTo(map);
 
 mainMarker.on('moveend', (evt) => {
-  document.querySelector('[name="address"]').value = evt.target.getLatLng();
+  document.querySelector('[name="address"]').value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
 });
 
-
-const points = getSimilarObjects();
 
 const icon = L.icon({
   iconUrl: './img/pin.svg',
@@ -56,19 +60,22 @@ const icon = L.icon({
 const markerGroup = L.layerGroup().addTo(map);
 
 const createMarker = (data) => {
-  const marker = L.marker({
-    lat: data.location.lat,
-    lng: data.location.lng
-  },
-  {
-    icon,
-  });
+  const markers = data.slice(0, 10);
+  markers.forEach((item) => {
+    const marker = L.marker({
+      lat: item.location.lat,
+      lng: item.location.lng
+    },
+    {
+      icon,
+    });
 
-  marker
-    .addTo(markerGroup)
-    .bindPopup(createCard(data));
+    marker
+      .addTo(markerGroup)
+      .bindPopup(createCard(item));
+  });
 };
 
-points.forEach((point) => {
-  createMarker(point);
-});
+getData(createMarker, onErrorGetServer);
+
+export {startLatData, startLngData, map, mainMarker};
