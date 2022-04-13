bc42ab4b325396a840da7378bfa804c3b16aa99e
Keks <keks@htmlacademy.ru>
2022-04-13 11:30:32 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/control-msg.js
@@ -0,0 +1,49 @@
+const serverErrorTemplate = document.querySelector('#server-error').content.querySelector('.server-error');
+const successPopupTemplate = document.querySelector('#success').content.querySelector('.success');
+const errorPopupTemplate = document.querySelector('#error').content.querySelector('.error');
+
+const closeClickMessagePopup = (target) => {
+  target.addEventListener('click', (evt) => {
+    if (evt.target === target || evt.target.matches('.error__button')) {
+      target.classList.remove('active');
+      document.querySelector('body').removeChild(target);
+    }
+  }, {once: true});
+};
+
+const closeEsckMessagePopup = (target) => {
+  document.addEventListener('keydown', (evt) => {
+    if(target.classList.contains('active')){
+      if (evt.key === 'Escape') {
+        target.classList.remove('active');
+        document.querySelector('body').removeChild(target);
+      }
+    }
+  }, {once: true});
+};
+
+const onErrorGetServer = () => {
+  const popupElement = serverErrorTemplate.cloneNode(true);
+  document.querySelector('body').append(popupElement);
+  popupElement.classList.add('active');
+  closeClickMessagePopup(popupElement);
+  closeEsckMessagePopup(popupElement);
+};
+
+const onSuccessPost = () => {
+  const popupElement = successPopupTemplate.cloneNode(true);
+  document.querySelector('body').append(popupElement);
+  popupElement.classList.add('active');
+  closeClickMessagePopup(popupElement);
+  closeEsckMessagePopup(popupElement);
+};
+
+const onErrorPost = () => {
+  const popupElement = errorPopupTemplate.cloneNode(true);
+  document.querySelector('body').append(popupElement);
+  popupElement.classList.add('active');
+  closeClickMessagePopup(popupElement);
+  closeEsckMessagePopup(popupElement);
+};
+
+export {onErrorGetServer, onSuccessPost, onErrorPost};
---
+++ 12/js/main.js
@@ -0,0 +1,9 @@
+import './form-validation.js';
+import './no-ui-slider.js';
+import './leaflet-map.js';
+import './image-preview.js';
+import './filter-map.js';
+import { listenUserForm, resetFormByButton } from './control-form.js';
+
+listenUserForm();
+resetFormByButton();
